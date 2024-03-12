622dfdd8d6769ca778afbeef347ef9a9036547cf
Ella Holl <ellaholl@Ellas-MacBook-Pro.local>
2024-03-11 22:07:30 -0400

    Connect  Map to WardrobeArea Interactable (first pass)

--- frontend/src/components/Town/TownGameScene.ts
+++ frontend/src/components/Town/TownGameScene.ts
@@ -9,6 +9,7 @@ import ConversationArea from './interactables/ConversationArea';
 import GameArea from './interactables/GameArea';
 import Transporter from './interactables/Transporter';
 import ViewingArea from './interactables/ViewingArea';
+import WardrobeArea from './interactables/WardrobeArea';
 
 // Still not sure what the right type is here... "Interactable" doesn't do it
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -21,6 +22,9 @@ function interactableTypeForObjectType(type: string): any {
     return ViewingArea;
   } else if (type === 'GameArea') {
     return GameArea;
+  }
+  else if (type === 'WardrobeArea') {
+    return WardrobeArea;
   } else {
     throw new Error(`Unknown object type: ${type}`);
   }
