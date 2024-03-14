aae80f67b6bb4d54a233c1c7873bbc24fc363327
arielmahler <arielmahler18@gmail.com>
2024-03-13 23:25:56 -0400

    Modified wardrobeArea to trigger interact when approached. Tested using same implementation on gameArea.

--- frontend/src/components/Town/interactables/WardrobeArea.ts
+++ frontend/src/components/Town/interactables/WardrobeArea.ts
@@ -1,7 +1,8 @@
 import Interactable, { KnownInteractableTypes } from '../Interactable';
+import WardrobeWrapper from './WardrobeArea';
 
 export default class WardrobeArea extends Interactable {
-  private _labelText?: Phaser.GameObjects.Text;
+  private _labelText?: JSX.Element;
 
   private _defaultVideoURL?: string;
 
@@ -11,29 +12,22 @@ export default class WardrobeArea extends Interactable {
     super.addedToScene();
     this.setTintFill();
     this.setAlpha(0.3);
-
-    this._labelText = this.scene.add.text(
+    this.setDepth(-1);
+    this.scene.add.text(
       this.x - this.displayWidth / 2,
-      this.y - this.displayHeight / 2,
-      `The wardrobe should appear here`,
+      this.y + this.displayHeight / 2,
+      this.name,
       { color: '#FFFFFF', backgroundColor: '#000000' },
     );
-    this._labelText.setVisible(false);
-    this.setDepth(-1);
   }
 
   overlap(): void {
-    if (!this._labelText) {
-      throw new Error('Should not be able to overlap with this interactable before added to scene');
-    }
-    const location = this.townController.ourPlayer.location;
-    this._labelText.setX(location.x);
-    this._labelText.setY(location.y);
-    this._labelText.setVisible(true);
+    this._isInteracting = true;
+    console.log('Entered wardrobe');
+    this.townController.interactableEmitter.emit('wardrobeArea', this);
   }
 
   overlapExit(): void {
-    this._labelText?.setVisible(false);
     if (this._isInteracting) {
       this.townController.interactableEmitter.emit('endInteraction', this);
       this._isInteracting = false;
@@ -41,8 +35,7 @@ export default class WardrobeArea extends Interactable {
   }
 
   interact(): void {
-    this._labelText?.setVisible(false);
-    this._isInteracting = true;
+    console.log('Interacting with wardrobe');
   }
 
   getType(): KnownInteractableTypes {
