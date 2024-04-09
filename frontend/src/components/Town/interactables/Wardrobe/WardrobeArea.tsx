9e2bdc218e5f9c747c91bfc45c9b25c580cde241
ryanchaput <99510232+ryanchaput@users.noreply.github.com>
2024-04-09 01:45:00 -0400

    Improving wardrobe UI

--- frontend/src/components/Town/interactables/Wardrobe/WardrobeArea.tsx
+++ frontend/src/components/Town/interactables/Wardrobe/WardrobeArea.tsx
@@ -4,7 +4,10 @@ import {
     AccordionIcon,
     AccordionItem,
     AccordionPanel,
+    Button,
+    IconButton,
     Box,
+    Container,
     Flex,
     Heading,
     List,
@@ -15,63 +18,108 @@ import {
     ModalContent,
     ModalHeader,
     ModalOverlay,
+    chakra,
   } from '@chakra-ui/react';
-  import React, { useCallback, useEffect, useState } from 'react';
-  import PlayerController from '../../../../classes/PlayerController';
-  import { useInteractable, useInteractableAreaController } from '../../../../classes/TownController';
-  import useTownController from '../../../../hooks/useTownController';
-  import { InteractableID } from '../../../../types/CoveyTownSocket';
-  import WardrobeAreaInteractable from '../WardrobeArea';
+import React, { useCallback, useEffect, useState } from 'react';
+import WardrobeAreaController from '../../../../classes/interactable/TownWardrobe/WardrobeAreaController';
+import PlayerController from '../../../../classes/PlayerController';
+import { useInteractable, useInteractableAreaController } from '../../../../classes/TownController';
+import useTownController from '../../../../hooks/useTownController';
+import { InteractableID } from '../../../../types/CoveyTownSocket';
+import WardrobeAreaInteractable from '../WardrobeArea';
 
+
+export const INVALID_GAME_AREA_TYPE_MESSAGE = 'Invalid game area type';
+
+const StyledOptionSquare = chakra(IconButton, {
+  baseStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexBasis: '45%',
+    border: '1px solid black',
+    height: '45%',
+    fontSize: '50px',
+    _disabled: {
+      opacity: '100%',
+    },
+  },
+});
+
+const StyledWardrobeBoard = chakra(Container, {
+  baseStyle: {
+    display: 'flex',
+    width: '400px',
+    height: '400px',
+    padding: '5px',
+    flexWrap: 'wrap',
+  },
+});
+
+function WardrobeBoard(controller: WardrobeAreaController): JSX.Element {
   
-  export const INVALID_GAME_AREA_TYPE_MESSAGE = 'Invalid game area type';
   
-  /**
-   * A generic component that renders a wardrobe area.
-   *
-   * It uses Chakra-UI components (does not use other GUI widgets)
-   *
-   * 
-   * TODO: What should it render?
-   */
-  function WardrobeArea({ interactableID }: { interactableID: InteractableID }): JSX.Element {
-    return (
-      <>
-      <h1>Wardrobe Area</h1>
-      </>
-    );
-  }
-  /**
-   * A wrapper component for the ConnectFourArea and TicTacToeArea components.
-   * Determines if the player is currently in a game area on the map, and if so,
-   * renders the selected game area component in a modal.
-   *
-   */
-  export default function WardrobeAreaWrapper(): JSX.Element {
-    const wardrobeArea = useInteractable<WardrobeAreaInteractable>('wardrobeArea');
-    const townController = useTownController();
+  return <></>
+}
 
-    const isOpen = wardrobeArea !== undefined;
+/**
+ * A generic component that renders a wardrobe area.
+ *
+ * It uses Chakra-UI components (does not use other GUI widgets)
+ *
+ * 
+ * TODO: What should it render?
+ */
+function WardrobeArea({ interactableID }: { interactableID: InteractableID }): JSX.Element {
+  const hairImage1 = React.createElement("img", {src: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'});
+  const hairImage2 = React.createElement("img", {src: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'});
+  const clothingImage1 = React.createElement("img", {src: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'});
+  const clothingImage2 = React.createElement("img", {src: 'https://www.pngitem.com/pimgs/m/146-1468479_my-profile-icon-blank-profile-picture-circle-hd.png'});
+  return (
+    <StyledWardrobeBoard>
+      <StyledOptionSquare aria-label='Hair Option 1' icon={hairImage1} />
+      <StyledOptionSquare aria-label='Hair Option 2' icon={hairImage2} />
+      <StyledOptionSquare aria-label='Clothing Option 1' icon={clothingImage1} />
+      <StyledOptionSquare aria-label='Clothing Option 2' icon={clothingImage2} />
+      <Button aria-label='Confirm Changes'>
+        Confirm Changes
+      </Button>
+    </StyledWardrobeBoard>
+  );
+}
+/**
+ * A wrapper component for the ConnectFourArea and TicTacToeArea components.
+ * Determines if the player is currently in a game area on the map, and if so,
+ * renders the selected game area component in a modal.
+ *
+ */
+export default function WardrobeAreaWrapper(): JSX.Element {
+  const wardrobeArea = useInteractable<WardrobeAreaInteractable>('wardrobeArea');
+  const townController = useTownController();
 
-    const closeModal = useCallback(() => {
-      if (wardrobeArea) {
-        townController.interactEnd(wardrobeArea);
-      }
-    }, [townController, wardrobeArea]);
+  const isOpen = wardrobeArea !== undefined;
+
+  const closeModal = useCallback(() => {
     if (wardrobeArea) {
-      //return <WardrobeArea wardrobeArea={wardrobeArea} />;
-      return (
-          <Modal isOpen={isOpen} closeOnOverlayClick={false} size='xl' onClose={() => {
-            closeModal();
-            townController.unPause();
-          }}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Wardrobe!</ModalHeader>
-            <ModalCloseButton />
-          </ModalContent>
-        </Modal>
-      )
+      townController.interactEnd(wardrobeArea);
     }
-    return <></>;
+  }, [townController, wardrobeArea]);
+  if (wardrobeArea) {
+    //return <WardrobeArea wardrobeArea={wardrobeArea} />;
+    return (
+        <Modal isOpen={isOpen} closeOnOverlayClick={false} size='xl' onClose={() => {
+          closeModal();
+          townController.unPause();
+        }}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Wardrobe: Edit your sprite's hair and outfit!</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <WardrobeArea interactableID={wardrobeArea.id} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    )
   }
+  return <></>;
+}
